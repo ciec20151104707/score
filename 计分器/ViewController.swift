1e9ef0be22954e48eabdff9b1eea02e234ec2db5
liyi <s20151104707@mac24.local>
2017-04-19 10:43:41 +0800

    可以简单的实现乒乓球比赛记分功能     用一个条件判断是不是在大于十分的情况下    最后输出哪个选手获胜

--- 计分器/ViewController.swift
+++ 计分器/ViewController.swift
@@ -7,9 +7,58 @@
 //
 
 import UIKit
-
+var a : Int = 0
+var b : Int = 0
+var x : Int = 0
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var x: UITextField!
+    @IBOutlet weak var y: UITextField!
+    @IBAction func restart(_ sender: UIButton) {
+        a = 0
+        b = 0
+        x.text = "\(0)"
+        y.text = "\(0)"
+    }
+    @IBAction func A1(_ sender: UIButton) {
+        a=a+1
+        x.text = "\(a)"
+        if a >= 10 && b >= 10{
+            if a >= b+2{
+                x.text = "A选手胜出"
+            }
+        }else if a > b && a == 11{
+            x.text="A选手获胜"
+        }
+        
+    }
+    @IBAction func A2(_ sender: UIButton) {
+        a=a-1
+        x.text = "\(a)"
+        if a < 0
+        {
+            x.text = "输入有误请点重新开始"}
+    }
+    @IBAction func B1(_ sender: UIButton) {
+        b=b+1
+        y.text = "\(b)"
+        if a >= 10 && b >= 10{
+            if a >= b+2{
+                x.text = "A选手胜出"
+            }
+        }else if b > a&&b == 11{
+            y.text="B选手获胜"
+        }
+    }
+    @IBAction func B2(_ sender: UIButton) {
+        b=b-1
+        y.text = "\(b)"
+        if b < 0
+        {
+            y.text = "输入有误请点重新开始"}
+    }
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
