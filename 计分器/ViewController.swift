ab731b5021602e0ea44a33f00c9f608e40c7c2d7
liyi <s20151104707@mac24.local>
2017-04-26 11:28:22 +0800

    4月26日
    增加俩个上传头像按钮，UIImagePickerControllerDelegate,
        UINavigationControllerDelegate增加这俩个头文件，能从相册中读取选手照片。

--- 计分器/ViewController.swift
+++ 计分器/ViewController.swift
@@ -10,12 +10,79 @@ import UIKit
 var a : Int = 0
 var b : Int = 0
 var x : Int = 0
-class ViewController: UIViewController {
+var flagA : Int = 0
+var flagB : Int = 0
+var dlg : Int = 0
+
+
+class ViewController: UIViewController,
+    UIImagePickerControllerDelegate,
+    UINavigationControllerDelegate{
 
 
     @IBOutlet weak var x: UITextField!
     @IBOutlet weak var y: UITextField!
     @IBOutlet weak var z: UITextField!
+    @IBOutlet weak var imageA: UIImageView!
+    @IBOutlet weak var image2: UIImageView!
+    @IBAction func img1(_ sender: UIButton) {
+        if dlg==0{
+            flagA=1
+            flagB=0
+            if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+                //初始化图片控制器
+                let picker = UIImagePickerController()
+                //设置代理
+                picker.delegate = self
+                //指定图片控制器类型
+                picker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+                //弹出控制器，显示界面
+                self.present(picker, animated: true, completion: {
+                    () -> Void in
+                })
+            }else{
+                print("读取相册错误")
+            }
+        }
+    }
+
+    @IBAction func img2(_ sender: UIButton) {
+        if dlg==0{
+            flagB=1
+            flagA=0
+            if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+                //初始化图片控制器
+                let picker = UIImagePickerController()
+                //设置代理
+                picker.delegate = self
+                //指定图片控制器类型
+                picker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+                //弹出控制器，显示界面
+                self.present(picker, animated: true, completion: {
+                    () -> Void in
+                })
+            }else{
+                print("读取相册错误")
+            }
+        }
+        
+    }
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any]) {
+        //查看info对象
+        print(info)
+        //显示的图片
+        let image:UIImage!
+        //获取选择的原图
+        image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        if(flagA==1){imageA.image = image}
+        else if(flagB==1){image2.image = image}
+        //图片控制器退出
+        picker.dismiss(animated: true, completion: {
+            () -> Void in
+        })
+    }
+    
     var i : Int = 0
     var j : Int = 0
     var k : Int = 0
@@ -50,7 +117,7 @@ class ViewController: UIViewController {
         
     }
     @IBAction func A2(_ sender: UIButton) {
-        a=a-1
+        a = a - 1
         x.text = "\(a)"
         if a < 0
         {
@@ -58,7 +125,7 @@ class ViewController: UIViewController {
         x.text = "/(0)"}
     }
     @IBAction func B1(_ sender: UIButton) {
-        b=b+1
+        b = b + 1
         y.text = "\(b)"
         if a >= 10 && b >= 10{
             if b >= a+2{
@@ -76,12 +143,13 @@ class ViewController: UIViewController {
         }
     }
     @IBAction func B2(_ sender: UIButton) {
-        b=b-1
+        b = b - 1
         y.text = "\(b)"
         if b < 0
         {
             y.text = "输入有误请点重新开始"
-        y.text = "\(0)"}
+            y.text = "\(0)"
+        }
     }
    
 
